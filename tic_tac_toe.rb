31f4638647db1e35f67b99a9d8e3c4af20571050
mjmoran123 <matthew.j.moran@gmail.com>
2016-10-04 17:00:54 -0500

    add move method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -7,9 +7,26 @@ class Game
 	end
 
 	def pretty_print
-		puts self.board[0..2] + "\n" + self.board[3..5] + "\n" + self.board[6..8]
+		puts self.board[0..2] + "\n" + self.board[3..5] + "\n" + self.board[6..8] + "\n\n"
 	end
+
+	def move(location)
+		if self.board[location] == "0"
+			if self.turn % 2 == 0
+				self.board[location] = "1"
+			else
+				self.board[location] = "2"
+			end
+			self.turn += 1
+		else
+			puts "location is occupied"
+		end
+		self.pretty_print
+	end
+
 end
 
 g = Game.new
 g.pretty_print
+g.move(1)
+g.move(8)
